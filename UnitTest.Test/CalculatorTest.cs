535eb7d77c72999581009521329b28be8e6c15c5
eymenburak <eymenburak@DESKTOP-5HNUA7V>
2021-05-29 17:24:45 +0300

    Arrange-Act-Assert

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTest.App;
+using Xunit;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            //Assert.Equal<int>(25, total);
+            Assert.NotEqual<int>(25, total);
+
+        }
+    }
+}
